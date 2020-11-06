50a5da56282b702801de63577102171e0bbee540
Vasilis Plavos <v@plavos.com>
2020-11-06 14:44:14 +0200

    finish test

--- dotnet/dotnet-gql-example/dotnet-gql-test/Startup.cs
+++ dotnet/dotnet-gql-example/dotnet-gql-test/Startup.cs
@@ -1,25 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using dotnet_gql_test.Data;
+using dotnet_gql_test.GQLQueries;
+using HotChocolate;
+using HotChocolate.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 
 namespace dotnet_gql_test
 {
@@ -37,19 +26,35 @@ namespace dotnet_gql_test
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddGraphQL(
+            //    SchemaBuilder.New()
+            //        // AddQueryType<T>() here
+            //        .Create());
+
             services.AddDbContext<MyDbContext>(opt =>
                 opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services
+                .AddDataLoaderRegistry()
+                .AddGraphQL(
+                    SchemaBuilder.New()
+                        .AddQueryType<LocationQueryType>()
+                        .Create());
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
 
-            app.UseRouting();
+
+            app
+                .UseRouting()
+                .UseWebSockets()
+                .UseGraphQL()
+                .UsePlayground();
 
             app.UseEndpoints(endpoints =>
             {
